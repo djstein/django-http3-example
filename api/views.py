9607d223b68ded86422903c0e78dd710e96035f2
Dylan Stein <djstein@ncsu.edu>
2023-01-04 11:12:49 -0800

    add example async django rest framework api

---
+++ api/views.py
@@ -0,0 +1,31 @@
+from adrf.views import APIView
+from django.contrib.auth.models import Group, User
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from api.serializers import GroupSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+
+class AsyncView(APIView):
+    async def get(self, request):
+        return Response({"message": "This is an async class based view."})
--- server/urls.py
+++ server/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    # path("", include(router.urls)),
+    path("", views.AsyncView.as_view()),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
